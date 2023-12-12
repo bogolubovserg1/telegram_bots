732377763227c9ce28bcabd3cbe503b1beebd335
bogolubovserg1 <bog2606@yandex.ru>
2023-12-13 01:15:50 +0300

    удалил

--- bot_api.py
+++ bot_api.py
@@ -3,6 +3,7 @@ from request import suma_ks, suma_for, suma_dor
 from aiogram.types import  KeyboardButton, InlineKeyboardMarkup, InlineKeyboardButton, Message, CallbackQuery
 
 
+
 access = '6645959837:AAHQHTobLoEQ7Pdz5Tg6sUYEFzy1sCPO4GQ'
 bot = ai.Bot(access)
 dp = ai.Dispatcher(bot)
@@ -10,7 +11,7 @@ suma_ks = '{:,}'.format(round(suma_ks, 2)).replace(",", " ")
 suma_for = '{:,}'.format(round(suma_for, 2)).replace(",", " ")
 suma_dor = '{:,}'.format(round(suma_dor, 2)).replace(",", " ")
 
-#забыл что комменты в питоне такие)))
+
 @dp.message_handler (commands = 'start')
 async def start(message: Message):
     await message.answer(f'Общая сумма КС-2: {suma_ks} руб.\nСумма КС-2 до выверки: {suma_for}руб.\nНа доработке: {suma_dor}руб.')
